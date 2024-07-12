d758fae5efd2c5d0925793ac8963d1b7af7847cf
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-11 21:56:49 -0400

    Class DataReading reads data.

---
+++ DataReading/DataReadingDemo.cs
@@ -0,0 +1,20 @@
+﻿using AoC2023;
+
+String inputPath = args[0];
+
+List<String> rawLines = DataReading.GetLinesFromFile(inputPath);
+
+Console.WriteLine("Raw Lines");
+foreach(string rawLine in rawLines)
+{
+    Console.WriteLine(rawLine);
+}
+
+List<int> linesNbWords = DataReading.ParseDataFromFile<int>(inputPath, str => str.Split(' ').Length);
+
+Console.WriteLine("\nNumber of Words");
+foreach(int lineNbWord in linesNbWords)
+{
+    Console.WriteLine(lineNbWord);
+}
+Console.WriteLine();
