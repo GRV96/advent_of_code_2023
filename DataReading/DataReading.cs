540374c20ac545688bce421111bb1beeb8a24be6
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2024-07-11 23:26:50 -0400

    Class DataReading reads text files (#1)

---
+++ DataReading/DataReading.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+namespace AoC2023
+{
+    internal class DataReading
+    {
+        public static List<String> GetLinesFromFile(String pFilePath)
+        {
+            List<String> lines = new List<String>();
+
+            using(StreamReader reader = new StreamReader(pFilePath))
+            {
+                String? line = null;
+
+                while((line = reader.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    lines.Add(line);
+                }
+            }
+
+            return lines;
+        }
+
+        public static List<T> ParseDataFromFile<T>(String pFilePath, Func<String, T> pParsing)
+        {
+            List<T> allData = new List<T>();
+
+            using (StreamReader reader = new StreamReader(pFilePath))
+            {
+                String? line = null;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    T parsedData = pParsing(line);
+                    allData.Add(parsedData);
+                }
+            }
+
+            return allData;
+        }
+    }
+}
