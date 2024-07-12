2599fe6d1eb3daa7bd5c6ce4a450e68621179c39
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-12 12:41:04 -0400

    Former application template

--- Day2/Day2.cs
+++ Day2/Day2.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace AoC2023
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
