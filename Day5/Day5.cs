d40426554cb8ab39244b9cc91a72aa8ec22d2e79
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-07-14 11:26:01 -0400

    Project Day5 prepared

---
+++ Day5/Day5.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC2023
+{
+    internal class Day5
+    {
+        static void Main(string[] args)
+        {
+            string inputPath = args[0];
+
+            using (StreamReader reader = new StreamReader(inputPath))
+            {
+                string? line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                }
+            }
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
