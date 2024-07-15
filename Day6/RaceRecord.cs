fdfc1c80e9a436ce2d81d051dddf51ea3e957c97
Guyllaume Rousseau <32419088+GRV96@users.noreply.github.com>
2024-07-14 23:45:44 -0400

    Day 6 (#7)
    Puzzle 1: 800280
    Puzzle 2: 45128024

---
+++ Day6/RaceRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace AoC2023
+{
+    internal struct RaceRecord
+    {
+        public readonly int allowedTime;
+        public readonly int distance;
+
+        public RaceRecord(int pAllowedTime, int pDistance)
+        {
+            allowedTime = pAllowedTime;
+            distance = pDistance;
+        }
+    }
+}
